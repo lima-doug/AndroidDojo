67c97dd84640951f957d1deb5f5a8e404649934a
lima-doug <get.douglaslima@gmail.com>
2022-10-28 10:34:03 -0300

    First app Android of the course - Desenvolvimento de Aplicativos Android usando Kotlin Udemy -

---
+++ TravelSpent/app/src/main/java/com/limadougg/gastoviagem/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.limadougg.gastoviagem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Toast
+import com.limadougg.gastoviagem.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonCalc.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_calc) {
+            calculate()
+        }
+
+    }
+
+    private fun isValid(): Boolean {
+        return (binding.editDistance.text.toString() != ""
+                && binding.editPrice.text.toString() != ""
+                && binding.editAutonomy.text.toString() != ""
+                && binding.editAutonomy.text.toString().toFloat() != 0f)
+    }
+
+
+    fun calculate() {
+        if (isValid()) {
+            val distance = binding.editDistance.text.toString().toFloat()
+            val price = binding.editPrice.text.toString().toFloat()
+            val autonomy = binding.editAutonomy.text.toString().toFloat()
+
+
+            val totalValue = (distance * price) / autonomy
+            binding.textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+
+        } else {
+
+                Toast.makeText(
+                    this,
+                    R.string.validation_fill_validation_all_fill,
+                    Toast.LENGTH_LONG
+                ).show()
+
+        }
+    }
+
+
+}
